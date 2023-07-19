ee2309899ee862ca52e5bc82838e12d3af5be67b
Ranjan Sharma <73110351+sharma3ranjan@users.noreply.github.com>
2023-07-19 22:48:20 +0530

    Project Structure

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css';
+import Nav from './components/Header/Nav';
+import Header from './components/Header/Header';
+import Services from './components/Services/Services';
+import WhoWeAre from './components/WhoWeAre/WhoWeAre';
+import Events from './components/Events/Events';
+import Gallery from './components/Events/Gallery';
+import Clients from './components/Clients/Clients';
+import Teams from './components/Team/Team';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
+
+function App() {
+  return (
+    <>
+      <div className='w-11/12 mx-auto justify-center items-center content-center'>
+        <Nav />
+        <Header />
+        <Services />
+        <WhoWeAre />
+        <Events />
+        <Gallery />
+        <Clients />
+        <Teams />
+        <Contact />
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
