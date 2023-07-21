f8606bfa39bbd996289b83b071c8f7301630f058
Ranjan Sharma <73110351+sharma3ranjan@users.noreply.github.com>
2023-07-21 23:54:36 +0530

    footer and contact section updated

--- src/components/Header/Nav.jsx
+++ src/components/Header/Nav.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import Logo from '../../assets/Logo.png';
 import { RiArrowDropDownLine } from 'react-icons/ri';
 
@@ -88,7 +88,7 @@ const Nav = () => {
 
             <li>
               <a
-                href='#'
+                href='#gallery'
                 className='block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
               >
                 Gallery
@@ -96,7 +96,7 @@ const Nav = () => {
             </li>
             <li>
               <a
-                href='#'
+                href='#ourclients'
                 className='block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
               >
                 Our Clients
@@ -104,7 +104,7 @@ const Nav = () => {
             </li>
             <li>
               <a
-                href='#'
+                href='#aboutus'
                 className='block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
               >
                 About Us
@@ -112,11 +112,59 @@ const Nav = () => {
             </li>
             <li>
               <a
-                href='#'
+                href='#contact'
                 className='block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
               >
                 Contact
               </a>
+            </li>
+            <li>
+              <a
+                // onMouseOver={displayDropdown2}
+                className='block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
+              >
+                Insights
+                <RiArrowDropDownLine className='inline text-[20px]' />
+              </a>
+              {/* <div
+                className={`${display2} flex flex-col absolute my-2 bg-gray-800 rounded`}
+              >
+                <a
+                  href='#events'
+                  onClick={displayNone2}
+                  className='text-white my-2 mx-2 text-[16px] font-semibold'
+                >
+                  Blogs
+                </a>
+                <a
+                  onClick={displayNone2}
+                  href='#events'
+                  className='text-white my-2 mx-2 text-[16px] font-semibold'
+                >
+                  Corporate Publication
+                </a>
+                <a
+                  onClick={displayNone2}
+                  href='#events'
+                  className='text-white my-2 mx-2 text-[16px] font-semibold'
+                >
+                  Podcast & Webinars
+                </a>
+                <a
+                  onClick={displayNone2}
+                  href='#events'
+                  className='text-white my-2 mx-2 text-[16px] font-semibold'
+                >
+                  Trainings & Certifications
+                </a>
+                <a
+                  onClick={displayNone2}
+                  href='#events'
+                  className='text-white my-2 mx-2 text-[16px] font-semibold'
+                >
+                  Reports & Insights
+                </a>
+              </div> */}
             </li>
           </ul>
         </div>
