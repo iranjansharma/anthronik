d092214464369ddc9c2b85c627c46a797634e5ee
Ranjan Sharma <73110351+sharma3ranjan@users.noreply.github.com>
2023-07-21 10:08:11 +0530

    Font Updated

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -7,7 +7,7 @@ const Events = () => {
             <h2 className='mb-4 text-3xl font-bold text-dark sm:text-4xl md:text-[40px] uppercase'>
               Our Events
             </h2>
-            <p className='text-[14px] lg:text-[20px] font-medium '>
+            <p className='text-[14px] lg:text-[18px] font-medium '>
               Explore our dynamic lineup of conferences, seminars, and summits
               that provide valuable opportunities for knowledge exchange,
               networking, and inspiration in the fields of Strategic
