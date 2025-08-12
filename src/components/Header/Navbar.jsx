f8885b103b313c8693a4c4620970b47d89a60b58
Ranjan Sharma <73110351+iranjansharma@users.noreply.github.com>
2025-08-12 19:20:19 +0530

    Update Navbar.jsx

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
         </svg>
       </button>
     </div>
-    <nav className={`flex-col flex-grow pb-4 md:pb-0 md:justify-end md:flex md:flex-row ${ open ? 'flex' : 'hidden'} className="flex-col flex-grow pb-4 md:pb-0 hidden md:flex md:justify-end md:flex-row">
+    <nav className={`flex-col flex-grow pb-4 md:pb-0 md:justify-end md:flex md:flex-row ${open ? 'flex' : 'hidden'}`}>
       <a className="px-4 py-2 mt-2 text-sm font-semibold text-gray-900 bg-gray-200 rounded-lg dark-mode:bg-gray-700 dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Blog</a>
       <a className="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Portfolio</a>
       <a className="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">About</a>
